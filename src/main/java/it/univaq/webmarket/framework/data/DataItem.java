e63d9d37316cb94d06e1b89a4c974fc36ef0f437
Lacele4 <168351055+Lacele4@users.noreply.github.com>
2025-05-06 02:23:51 +0200

    Implementata DataItem

--- src/main/java/it/univaq/webmarket/framework/data/DataItem.java
+++ src/main/java/it/univaq/webmarket/framework/data/DataItem.java
@@ -2,12 +2,14 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
  */
+
 package it.univaq.webmarket.framework.data;
 
 /**
  *
  * @author cdidi
  * @param <KT> the key type
+ * 
  */
 
 public interface DataItem<KT> {
